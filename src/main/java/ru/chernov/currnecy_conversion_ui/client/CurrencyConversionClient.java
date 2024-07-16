84baf043c85bb37f50a5d2d6f423be31f0ab36f5
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-16 18:53:35 +0300

    Add method that gets currency codes from API

--- src/main/java/ru/chernov/currnecy_conversion_ui/client/CurrencyConversionClient.java
+++ src/main/java/ru/chernov/currnecy_conversion_ui/client/CurrencyConversionClient.java
@@ -6,11 +6,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ru.chernov.currnecy_conversion_ui.dto.CurrencyCodesResponse;
 import ru.chernov.currnecy_conversion_ui.dto.CurrencyConversionResponse;
 
 @FeignClient(name = "currency-conversion-client", url = "${currency-coversion-api.url}")
 public interface CurrencyConversionClient {
     @GetMapping("/convert")
     public CurrencyConversionResponse convertCurrency(@RequestParam("from") String from, @RequestParam("to") String to, @RequestParam("amount") BigDecimal amount);
-    
+
+    @GetMapping("/codes") 
+    public CurrencyCodesResponse getCurrencyCodes();
 } 
