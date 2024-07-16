db503a6aa92082f05ee391a780b04bcaecb6ab37
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-16 17:40:34 +0300

    Add currency conversion client

--- src/main/java/ru/chernov/currnecy_conversion_ui/CurrnecyConversionUiApplication.java
+++ src/main/java/ru/chernov/currnecy_conversion_ui/CurrnecyConversionUiApplication.java
@@ -2,8 +2,10 @@ package ru.chernov.currnecy_conversion_ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrnecyConversionUiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/ru/chernov/currnecy_conversion_ui/client/CurrencyConversionClient.java
@@ -0,0 +1,19 @@
+package ru.chernov.currnecy_conversion_ui.client;
+
+import java.math.BigDecimal;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ru.chernov.currnecy_conversion_ui.dto.CurrencyConversionResponse;
+
+@FeignClient(name = "currency-conversion-client", url = "${currency-coversion-api.url}")
+public interface CurrencyConversionClient {
+    @GetMapping("/convert")
+    public CurrencyConversionResponse convertCurrency(
+        @RequestParam("from") String from,
+        @RequestParam("to") String to,
+        @RequestParam("amount") BigDecimal amount
+    );
+} 
