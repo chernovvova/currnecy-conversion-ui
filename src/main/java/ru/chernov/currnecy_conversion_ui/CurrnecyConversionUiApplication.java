dd2b06ca1c67879de1234ce1cdb5cbcd62e35a41
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-16 15:29:24 +0300

    Initial commit

---
+++ src/main/java/ru/chernov/currnecy_conversion_ui/CurrnecyConversionUiApplication.java
@@ -0,0 +1,13 @@
+package ru.chernov.currnecy_conversion_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrnecyConversionUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrnecyConversionUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/chernov/currnecy_conversion_ui/CurrnecyConversionUiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chernov.currnecy_conversion_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrnecyConversionUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
